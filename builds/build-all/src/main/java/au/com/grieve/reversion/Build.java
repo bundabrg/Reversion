84913120f34f8aa65629ac9743eb8a9000c44e3a
bundabrg <bundabrg@grieve.com.au>
2020-09-17 15:33:50 +0800

    Update Copyright and implement Build Packages

---
+++ builds/build-all/src/main/java/au/com/grieve/reversion/Build.java
@@ -0,0 +1,47 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Reversion Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package au.com.grieve.reversion;
+
+import au.com.grieve.reversion.api.RegisteredTranslator;
+import au.com.grieve.reversion.translators.v390ee_to_v408be.Register_v390ee_to_v408be;
+import au.com.grieve.reversion.translators.v409be_to_v408be.Register_v409be_to_v408be;
+import au.com.grieve.reversion.translators.v411be_to_v409be.Register_v411be_to_v409be;
+import au.com.grieve.reversion.translators.v412be_to_v411be.Register_v412be_to_v411be;
+import au.com.grieve.reversion.translators.v414be_to_v412be.Register_v414be_to_v412be;
+import lombok.experimental.UtilityClass;
+
+/**
+ * Utility Class that lists resources in this build
+ */
+@UtilityClass
+public class Build {
+    public static final RegisteredTranslator[] TRANSLATORS = {
+            Register_v409be_to_v408be.TRANSLATOR,
+            Register_v411be_to_v409be.TRANSLATOR,
+            Register_v390ee_to_v408be.TRANSLATOR,
+            Register_v412be_to_v411be.TRANSLATOR,
+            Register_v414be_to_v412be.TRANSLATOR
+    };
+}
